962a3f89eb40b381c90f8a3f597d20b46cd40ad1
Leonardo Velasquez <106557283+5kleo8@users.noreply.github.com>
2023-08-28 17:21:59 -0500

    Add files via upload

---
+++ Portfolio/Javascript/Index.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+    // Obtiene el elemento h1
+    const h1Element = document.querySelector('#hero h1');
+
+    // Aplica los estilos para mostrar el h1 con una transición
+    h1Element.style.opacity = '1';
+    h1Element.style.transform = 'translateY(0)';
+  });
